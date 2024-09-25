278c8c64010f2f9e97feee265ffe586ec117fa3d
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-09-25 20:04:15 +0300

    уу

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import Loader from './components/Loader/Loader';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';  
 import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';  
 import ImageModal from './components/ImageModal/ImageModal';  
-import { Toaster, toast } from 'react-hot-toast'; // Правильний імпорт  
+import { Toaster, toast } from 'react-hot-toast'; 
 
 
 const API_URL = 'https://api.unsplash.com/photos/';  
-const ACCESS_KEY = '3wbuu2ZWzl1pTV4tbxDtcjf-xJZKkSCoetWrEiQhft4'; // Заміни на свій Access Key  
+const ACCESS_KEY = '3wbuu2ZWzl1pTV4tbxDtcjf-xJZKkSCoetWrEiQhft4';
 
 const App = () => {  
   const [query, setQuery] = useState('');  
@@ -21,7 +21,7 @@ const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);  
   const [selectedImage, setSelectedImage] = useState(null);  
 
-  // Завантаження зображень  
+ 
   const loadImages = async (searchQuery, pageNumber) => {  
     setLoading(true);  
     setError(null);  
@@ -30,17 +30,17 @@ const App = () => {
       const response = await axios.get(`${API_URL}?query=${searchQuery}&page=${pageNumber}&client_id=${ACCESS_KEY}`);  
       setImages(prevImages => [...prevImages, ...response.data]);  
       if (response.data.length === 0) {  
-        toast.error('No images found for your search.'); // Додайте сповіщення  
+        toast.error('No images found for your search.');   
       }  
     } catch (error) {  
       setError('Error fetching images. Please try again later.');  
-      toast.error('Error fetching images. Please try again later.'); // Сповіщення про помилку  
+      toast.error('Error fetching images. Please try again later.'); 
     } finally {  
       setLoading(false);  
     }  
   };  
 
-  // Обробка запиту на пошук  
+
   const handleSearch = query => {  
     setQuery(query);  
     setImages([]);  
@@ -48,13 +48,13 @@ const App = () => {
     loadImages(query, 1);  
   };  
 
-  // Завантаження наступної порції зображень  
+  
   const loadMoreImages = () => {  
     setPage(prevPage => prevPage + 1);  
     loadImages(query, page + 1);  
   };  
 
-  // Відкриття модального вікна  
+  
   const openModal = image => {  
     setSelectedImage(image);  
     setIsModalOpen(true);  
@@ -66,7 +66,7 @@ const App = () => {
     setSelectedImage(null);  
   };  
 
-  // Ефект для завантаження зображень після зміни запиту  
+
   useEffect(() => {  
     if (query) {  
       loadImages(query, page);  
@@ -75,7 +75,7 @@ const App = () => {
 
   return (  
     <div>  
-      <Toaster />  {/* Використання Toaster на верху компонента */}  
+      <Toaster />  
       <SearchBar onSubmit={handleSearch} />  
       {error ? (  
         <ErrorMessage message={error} />  
